0d0e2d159ad6ab1251a417492315c4dcd176f4b5
Josh31415 <jobrud314@gmail.com>
2017-10-01 13:49:21 +0000

    Finished first working model

---
+++ ClassifyDigits.py
@@ -0,0 +1,96 @@
+import csv
+from Model import Model
+import tensorflow as tf
+
+class ClassifyDigits():
+
+  def parseTrainingData(self, csvFile, imageSize):
+    data = []
+    labels = []
+    reader = csv.reader(csvFile)
+
+    for row in reader:
+      if(len(data) <= 1000):
+        floatRow = []
+        labels.append(int(row[0]))
+        for i in range(1, len(row)):
+          floatRow.append(float(row[i]))
+       
+        image = []
+        for i in range(0, 28):
+          index = imageSize * i
+          imageRow = []
+          for j in range(0, 28):
+            imageRow.append([floatRow[index + j]])
+          image.append(imageRow)
+        data.append(image)
+      else:
+        break
+
+    return labels, data
+
+  def convertImage(imageName):
+    imageFile = tf.read_file(im_dir+im_name)
+    image = tf.image.decode_png(im_content, channels=3)
+    image = tf.cast(image, tf.float32) / 255.
+    image = tf.image.resize_images(image, 28, 28)
+  
+    return image
+
+  def train(self):
+    model = Model()
+    trainFile = open("mnist_train.csv")
+    testFile = open("mnist_test.csv")
+
+    trainingLabels, trainingData = self.parseTrainingData(trainFile, 28)
+    testingLabels, testingData = self.parseTrainingData(testFile, 28) 
+ 
+    #model.initModel(30, 3)
+    model.trainModel(trainingData, testingData, trainingLabels, testingLabels)
+ 
+
+  def imageToFloat(self, images):
+    floatImages = []
+    for i in range(0, len(images)):
+      newimage = []
+      for j in range(0, len(images[i])):
+        imageRow = []
+        for k in range(0, len(images[i][j])):
+          imageRow.append([abs(float(images[i][j][k]) - 255)])
+        newimage.append(imageRow)
+      floatImages.append(newimage)
+    return floatImages
+ 
+  def classifyDigitArray(self, images):
+    model = Model()
+    #model.initModel(30, 2)
+    self.train()
+    newImages = self.imageToFloat(images)
+
+    probList = [] 
+    for i in range(0, len(newImages)):
+      numbers = model.classifyImage([newImages[i]])
+      session = tf.Session()
+      init = tf.global_variables_initializer()
+      session.run(init)
+      probList.append(session.run(numbers).tolist()[0])
+      print(i) 
+
+    print(probList) 
+    foundNumbers = []
+    for i in range(0, len(probList)):
+      highProb = 0.0
+      probIndex = 0
+      for j in range(0, len(probList[i])):
+        if probList[i][j] > highProb:
+          print("High")
+          print(probList[i][j])
+          hightProb = probList[i][j]
+          probIndex = j + 1
+      if highProb == 0.0:
+        foundNumbers.append(0) 
+      else:
+        foundNumbers.append(probIndex) 
+      
+    print(foundNumbers)
+    return foundNumbers
--- Model.py
+++ Model.py
@@ -4,40 +4,28 @@ import os
 
 class Model():
   layerDepth = 30
-  convolutionLayers = 1
+  convolutionLayers = 2
   filterDimentions = [2, 2, 1, 1]
   filters = tf.Variable(tf.truncated_normal(filterDimentions, stddev=0.03)) 
   bias = tf.Variable(tf.truncated_normal([30], stddev=0.03)) 
+  init = tf.global_variables_initializer()
   
   #silences tensorflow info logging
   os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+  sess = tf.Session()
 
   def initModel(self, depth, layers): 
+    saver = tf.train.import_meta_graph("./digitClassificationModel.meta")
+    saver.restore(self.sess, tf.train.latest_checkpoint('./'))
+    graph = tf.get_default_graph()
+    self.filters = graph.get_tensor_by_name("filters:0")
+    self.bias = graph.get_tensor_by_name("biases")
+
+    self.sess.run(self.init)
     self.layerDepth = depth
     self.convolutionLayers = layers
   
-  def initFilters(self):
-    initialFilters = []
-    for i in range(0, self.convolutionLayers):
-      filt = []
-      filt.append([[[random.random()]], [[random.random()]]])
-      filt.append([[[random.random()]], [[random.random()]]])
-      initialFilters.append(filt) 
-    print(initialFilters)
-    self.filters= tf.Variable(initialFilters)
-    init = tf.global_variables_initializer()
-    self.sess.run(init)
-    self.sess.run(self.filters)
-
-  def dotProduct(kernel, imageSeg):
-    dotProd = 0
-    if len(kernel) == len(imageSeq):
-      for i in range(0, len(kernel)):
-        for j in range(0, len(kernel[i])):
-          dotProd += kernel[i][j] * imageSeq[i][j]
-    return dotProd
-
-  def classifyImage(self, data, imageLabel):
+  def classifyImage(self, data):
     image = tf.Variable(data, name="image")
 
     firstConvLayer = tf.nn.conv2d(
@@ -49,25 +37,29 @@ class Model():
     firstConvRelu = tf.nn.relu(firstConvLayer)
     convResult = tf.layers.max_pooling2d(inputs=firstConvRelu, pool_size=[2, 2], strides=2)
 
-    for i in range(0, self.convolutionLayers - 2):
+    for i in range(0, self.convolutionLayers - 1):
       convLayer = tf.nn.conv2d(
-        image,
+        convResult,
         self.filters,
         strides=[1, 2, 2, 1],
         padding="SAME"
       )
-      
       reluStep = tf.nn.relu(convLayer)
       poolLayer = tf.layers.max_pooling2d(inputs=reluStep, pool_size=[2, 2], strides=2)
       convResult = poolLayer
 
     # image flattening layer
-    pool2_flat = tf.reshape(convResult, [-1, 49])
+    pool2_flat = tf.reshape(convResult, [-1, 4])
 
     # fully connected layer
     dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
     dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=True)
     probabilities = tf.layers.dense(inputs=dropout, units=10)
+
+    #session = tf.Session()
+    #init = tf.global_variables_initializer()
+    #session.run(init)
+    #print(session.run(probabilities))
 
     return probabilities
 
@@ -83,12 +75,11 @@ class Model():
   
   def trainModel(self, trainData, testData, trainLabels, testLabels):
     loss = 0
-    sess = tf.Session()
 
-    for i in range(0, 5):
+    for i in range(0, 8):
       print(i)
       batchLabels, batchData = self.getBatchNum(i + 1, 100, trainData, trainLabels)
-      yhat = self.classifyImage(batchData, batchLabels)
+      yhat = self.classifyImage(batchData)
       onehot = tf.one_hot(indices=tf.cast(batchLabels, tf.int32), depth=10)
       loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot, logits=yhat)
 
@@ -98,15 +89,13 @@ class Model():
         learning_rate=0.1,
         optimizer="SGD") 
 
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    saver = tf.train.Saver()
-    saver.save(sess, "digitClassificationModel")
+    self.sess.run(self.init)
+    saver = tf.train.Saver({"filters:0": self.filters})
+    saver.save(self.sess, "digitClassificationModel")
 
     testLoss = 0
     for i in range(0, 1):
       batchLabels, batchData = self.getBatchNum(i + 1, 100, testData, testLabels)
-      yhat = self.classifyImage(batchData, batchLabels)
+      yhat = self.classifyImage(batchData)
       onehot = tf.one_hot(indices=tf.cast(testLabels[i : i + 1], tf.int32), depth=10)
       testLoss = tf.losses.softmax_cross_entropy(onehot_labels=onehot, logits=yhat)
-      print(i)
